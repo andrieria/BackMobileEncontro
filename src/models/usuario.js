76a58a6ebd3d99fedfa486d005e25c17ac198e63
Kelvin Marques <kelvinmarquesk@gmail.com>
2024-08-09 19:18:59 -0300

    feat: Modelo usuario

---
+++ src/models/usuario.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Usuario = sequelize.define('Usuario', {
+        id: { type: DataTypes.INTEGER, primarykey: true, autoIncrement: true },
+        email: { type: DataTypes.STRING, allowNull: false, unique: true },
+        senha: { type: DataTypes.STRING, allowNull: false },
+        nome_completo: { type: DataTypes.STRING, allowNull: false },
+        cpf: { type: DataTypes.STRING(11), allowNull: false, unique: true },
+        data_nascimento: { type: DataTypes.STRING, allowNull: false },
+        telefone: DataTypes.STRING,
+        rua: DataTypes.STRING,
+        bairro: DataTypes.STRING,
+        cidade: DataTypes.STRING,
+        estado: DataTypes.STRING(2),
+        cep: DataTypes.STRING(8),
+        sexo: DataTypes.ENUM('M', 'F', 'Outro'),
+        nivel_escolaridade: DataTypes.STRING,
+    });
+}
