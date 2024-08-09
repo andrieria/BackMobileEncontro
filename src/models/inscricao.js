87dba9c90fc33dc8beaeb9c6ba0ebdf64291bde6
Guilherme-Aurelio <guilhermeaurelio65@gmail.com>
2024-08-09 19:27:17 -0300

    inscrição

---
+++ src/models/inscricao.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Usuario = sequelize.define('Usuario', {
+        id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        usuario_id: {type: DataTypes.INTEGER, references: {model: 'usuario', key: 'id'}, onUpdate: 'CASCADE', onDelete: 'SET NULL'},
+        evento_id: {type: DataTypes.INTEGER, references: {model: 'evento', key: 'id'}, onUpdate: 'CASCADE',onDelete: 'SET NULL'},
+        status: {type: DataTypes.ENUM('Deferido', 'Indeferido', 'Em Análise'),allowNull: false},
+        administrador_id: {type: DataTypes.INTEGER,references: {model: 'administrador',key: 'id'}, onUpdate: 'CASCADE', onDelete: 'SET NULL'},
+        created_at: {type: DataTypes.DATE,allowNull: false,defaultValue: DataTypes.NOW},
+        updated_at: { type: DataTypes.DATE,allowNull: false, defaultValue: DataTypes.NOW}
+    });
+}
