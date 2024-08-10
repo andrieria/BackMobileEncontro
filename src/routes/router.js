2fac9bda11bff5801903d8c183fbe537e29be04b
Guilherme-Aurelio <guilhermeaurelio65@gmail.com>
2024-08-09 21:16:23 -0300

    usuariocontroller

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const AdministradorController = require('../controllers/administradorController');
+const usuarioController = require('../controllers/usuarioController');
 
+router.post('/cadastrar', usuarioController.cadastrar);
+router.post('/login', usuarioController.login);
 router.post('/administradores', AdministradorController.create);
 router.get('/administradores', AdministradorController.index);
 router.get('/administradores/:id', AdministradorController.show);
